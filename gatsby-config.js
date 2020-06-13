a5749994c46b3cefcd277cc5c85b68a17893ec3f
Wandu <d_wandz@yahoo.com>
2020-06-13 13:03:24 +0200

    added responsiveness

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,28 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Huddle landing page`,
+    description: `Huddle landing page built in gatsby`,
+    author: `@sMit3r911`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Poppins\:400,700`,
+          `Open Sans\:400` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    },
+    `gatsby-plugin-sharp`, 
+    `gatsby-transformer-sharp`,
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,49 @@
 import React from "react"
+import Layout from "../components/layout/layout"
+import logo from "../../static/logo.svg"
+import mockUp from "../../static/illustration-mockups.svg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebookF, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+
+export default function Home({data}) {
+  return (
+    <div className="container">
+        <img src={logo} alt="logo"></img>
+      <div className="flex">
+				<div>
+					<img src={mockUp} alt="illustration" />
+				</div>
+
+				<div>
+					<h1>Build The Community Your Fans Will Love</h1>
+					<p>
+						Huddle re-imagines the way we build communities. You have a voice,
+						but so does your audience. Create connections with your users as you
+						engage in genuine discussion.
+					</p>
+					<button>Register</button>
+				</div>
+			</div>
+
+      <ul className="social-media">
+				<li>
+					<a href="#">
+						<FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+          <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+          <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+					</a>
+				</li>
+			</ul>
+      </div>
+  )
 }
